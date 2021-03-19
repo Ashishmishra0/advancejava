ff5d11ab7140fed4e7fb3d358fa7c576c295aa76
Ashishmishra0 <80473475+Ashishmishra0@users.noreply.github.com>
2021-03-19 10:24:59 +0530

    Create JFrameDemo.java

---
+++ JFrameDemo.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+class JframeDemo {
+    public static void main(String[] args) {
+        JFrame ob2= new JFrame("alfa's's Frame");
+        ob2.setSize(280,300);
+        ob2.setVisible(true);
+    }
+}
